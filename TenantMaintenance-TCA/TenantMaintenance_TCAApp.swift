de50b6ce770d8ecc08c9c58aa0f3549693c8bf30
kurtcodina <128434639+kurtcodina@users.noreply.github.com>
2025-10-05 15:27:45 +0800

    Initial Commit

---
+++ TenantMaintenance-TCA/TenantMaintenance_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TenantMaintenance_TCAApp.swift
+//  TenantMaintenance-TCA
+//
+//  Created by codina on 10/5/25.
+//
+
+import SwiftUI
+
+@main
+struct TenantMaintenance_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
